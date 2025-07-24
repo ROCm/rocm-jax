78ba9a0a5ecc34105aeb45a3d5d11c41df221e5b
Ruturaj4 <Ruturaj.Vaidya@amd.com>
2025-07-24 01:50:22 +0000

    Add gfx950 to build.py

--- jax_rocm_plugin/build/build.py
+++ jax_rocm_plugin/build/build.py
@@ -233,7 +233,7 @@ def add_artifact_subcommand_arguments(parser: argparse.ArgumentParser):
     rocm_group.add_argument(
         "--rocm_amdgpu_targets",
         type=str,
-        default="gfx906,gfx908,gfx90a,gfx942,gfx1030,gfx1100,gfx1101,gfx1200,gfx1201",
+        default="gfx906,gfx908,gfx90a,gfx942,gfx950,gfx1030,gfx1100,gfx1101,gfx1200,gfx1201",
         help="A comma-separated list of ROCm amdgpu targets to support.",
     )
 
