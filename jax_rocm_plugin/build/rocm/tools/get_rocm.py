0b4bcbf1d55465365f577d8da0ceab310bbd1755
Juha Jäykkä <juha.jaykka@amd.com>
2025-09-19 08:39:52 +0000

    standards compliance: %d for base-10 format strings

--- jax_rocm_plugin/build/rocm/tools/get_rocm.py
+++ jax_rocm_plugin/build/rocm/tools/get_rocm.py
@@ -423,8 +423,8 @@ gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
             """
 [amdgpu]
 name=amdgpu
-baseurl=https://repo.radeon.com/%s/%s/rhel/8.%i/main/x86_64/
-enabled=1
+baseurl=https://repo.radeon.com/%s/%s/rhel/8.%d/main/x86_64/
+senabled=1
 gpgcheck=1
 gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
 """
