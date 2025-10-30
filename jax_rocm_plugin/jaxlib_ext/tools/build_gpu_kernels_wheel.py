f10dbb67e9d738e2dd01b0d916afe4db2473bdd7
Zahid Iqbal <149706611+zahiqbal@users.noreply.github.com>
2025-10-29 21:25:38 -0500

    Adding rpath for /opt/rocm/libs, remove --force-rpath to enable user to set LD_LIBRARY_PATH (#159)

--- jax_rocm_plugin/jaxlib_ext/tools/build_gpu_kernels_wheel.py
+++ jax_rocm_plugin/jaxlib_ext/tools/build_gpu_kernels_wheel.py
@@ -12,10 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Script that builds a jax-rocm-plugin wheel for ROCm kernels.
+# Most users should not run this script directly; use build.py instead.
+# pylint: disable=duplicate-code
 
-This script is intended to be run via bazel run as part of the JAX ROCm plugin
-build process. Most users should not run this script directly; use build.py instead.
+"""
+Script to build a JAX ROCm kernel plugin wheel. Intended for use via Bazel.
 """
 
 import argparse
@@ -32,6 +33,12 @@ from bazel_tools.tools.python.runfiles import runfiles
 from jaxlib_ext.tools import build_utils
 
 parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--sources_path",
+    default=None,
+    help="Path in which the wheel's sources should be prepared. Optional. If "
+    "omitted, a temporary directory will be used.",
+)
 parser.add_argument(
     "--output_path",
     default=None,
@@ -98,7 +105,7 @@ parser.add_argument(
 args = parser.parse_args()
 
 r = runfiles.Create()
-pyext = "pyd" if build_utils.is_windows() else "so"
+PYEXT = "pyd" if build_utils.is_windows() else "so"
 
 
 def write_setup_cfg(setup_sources_path, cpu):
@@ -159,14 +166,14 @@ def prepare_wheel_rocm(wheel_sources_path: pathlib.Path, *, cpu, rocm_version):
     copy_runfiles(
         dst_dir=plugin_dir,
         src_files=[
-            f"jax/jaxlib/rocm/_linalg.{pyext}",
-            f"jax/jaxlib/rocm/_prng.{pyext}",
-            f"jax/jaxlib/rocm/_solver.{pyext}",
-            f"jax/jaxlib/rocm/_sparse.{pyext}",
-            f"jax/jaxlib/rocm/_hybrid.{pyext}",
-            f"jax/jaxlib/rocm/_rnn.{pyext}",
-            f"jax/jaxlib/rocm/_triton.{pyext}",
-            f"jax/jaxlib/rocm/rocm_plugin_extension.{pyext}",
+            f"jax/jaxlib/rocm/_linalg.{PYEXT}",
+            f"jax/jaxlib/rocm/_prng.{PYEXT}",
+            f"jax/jaxlib/rocm/_solver.{PYEXT}",
+            f"jax/jaxlib/rocm/_sparse.{PYEXT}",
+            f"jax/jaxlib/rocm/_hybrid.{PYEXT}",
+            f"jax/jaxlib/rocm/_rnn.{PYEXT}",
+            f"jax/jaxlib/rocm/_triton.{PYEXT}",
+            f"jax/jaxlib/rocm/rocm_plugin_extension.{PYEXT}",
             "jax/jaxlib/version.py",
         ],
     )
@@ -189,16 +196,16 @@ def prepare_wheel_rocm(wheel_sources_path: pathlib.Path, *, cpu, rocm_version):
         raise RuntimeError(mesg) from ex
 
     files = [
-        f"_linalg.{pyext}",
-        f"_prng.{pyext}",
-        f"_solver.{pyext}",
-        f"_sparse.{pyext}",
-        f"_hybrid.{pyext}",
-        f"_rnn.{pyext}",
-        f"_triton.{pyext}",
-        f"rocm_plugin_extension.{pyext}",
+        f"_linalg.{PYEXT}",
+        f"_prng.{PYEXT}",
+        f"_solver.{PYEXT}",
+        f"_sparse.{PYEXT}",
+        f"_hybrid.{PYEXT}",
+        f"_rnn.{PYEXT}",
+        f"_triton.{PYEXT}",
+        f"rocm_plugin_extension.{PYEXT}",
     ]
-    runpath = "$ORIGIN/../rocm/lib:$ORIGIN/../../rocm/lib"
+    runpath = "$ORIGIN/../rocm/lib:$ORIGIN/../../rocm/lib:/opt/rocm/lib"
     # patchelf --force-rpath --set-rpath $RUNPATH $so
     for f in files:
         so_path = os.path.join(plugin_dir, f)
@@ -207,30 +214,42 @@ def prepare_wheel_rocm(wheel_sources_path: pathlib.Path, *, cpu, rocm_version):
         if not perms & stat.S_IWUSR:
             fix_perms = True
             os.chmod(so_path, perms | stat.S_IWUSR)
-        subprocess.check_call(
-            ["patchelf", "--force-rpath", "--set-rpath", runpath, so_path]
-        )
+        subprocess.check_call(["patchelf", "--set-rpath", runpath, so_path])
         if fix_perms:
             os.chmod(so_path, perms)
 
 
-tmpdir = tempfile.TemporaryDirectory(prefix="jax_rocm_plugin")
-sources_path = tmpdir.name
-try:
-    os.makedirs(args.output_path, exist_ok=True)
-    prepare_wheel_rocm(
-        pathlib.Path(sources_path), cpu=args.cpu, rocm_version=args.platform_version
-    )
-    package_name = f"jax rocm{args.platform_version} plugin"
-    if args.editable:
-        build_utils.build_editable(sources_path, args.output_path, package_name)
-    else:
-        git_hash = build_utils.get_githash(args.rocm_jax_git_hash)
-        build_utils.build_wheel(
-            sources_path,
-            args.output_path,
-            package_name,
-            git_hash=git_hash,
-        )
-finally:
-    tmpdir.cleanup()
+def main():
+    """Main entry point for building the ROCm plugin wheel."""
+    sources_path = args.sources_path
+    with tempfile.TemporaryDirectory(prefix="jax_rocm_plugin") as tmpdir:
+
+        if sources_path is None:
+            sources_path = tmpdir
+
+        os.makedirs(args.output_path, exist_ok=True)
+
+        if args.enable_rocm:
+            prepare_wheel_rocm(
+                pathlib.Path(sources_path),
+                cpu=args.cpu,
+                rocm_version=args.platform_version,
+            )
+            package_name = f"jax rocm{args.platform_version} plugin"
+        else:
+            raise ValueError("Unsupported backend. Choose 'rocm'.")
+
+        if args.editable:
+            build_utils.build_editable(sources_path, args.output_path, package_name)
+        else:
+            git_hash = build_utils.get_githash(args.rocm_jax_git_hash)
+            build_utils.build_wheel(
+                sources_path,
+                args.output_path,
+                package_name,
+                git_hash=git_hash,
+            )
+
+
+if __name__ == "__main__":
+    main()
