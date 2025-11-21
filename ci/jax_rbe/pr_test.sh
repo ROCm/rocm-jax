c25329a6a43fe87c781784de55410466db2f2104
Charles Hofer <Charles.Hofer@amd.com>
2025-11-21 16:06:58 +0000

    Run different tests

--- ci/jax_rbe/pr_test.sh
+++ ci/jax_rbe/pr_test.sh
@@ -44,8 +44,9 @@ python3 build/build.py build --wheels=jax-rocm-plugin --configure_only --python_
     --test_output=errors \
     //tests:core_test_gpu \
     //tests:linalg_test_gpu \
+    //tests:ffi_test_gpu \
     --test_filter=CoreTest \
     --test_filter=JaxprTypeChecks \
     --test_filter=DynamicShapesTest \
-    --test_filter=testMatmul
+    --test_filter=ScipyLinalgTest
 
