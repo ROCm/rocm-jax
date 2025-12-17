3b0625610fac9fe4a19be327d459c2251eab344a
cj401-amd <chunyjin@amd.com>
2025-12-17 07:23:40 -0600

    update to make hipInit() happen after rocprofiler_configure() in the pjrt plugin

--- jax_rocm_plugin/pjrt/python/__init__.py
+++ jax_rocm_plugin/pjrt/python/__init__.py
@@ -139,16 +139,17 @@ def initialize():
     if rocm_plugin_extension is None:
         logger.warning("rocm_plugin_extension not found")
         return
-
-    device_count = rocm_plugin_extension.get_device_count()
-    if device_count <= 0:
-        raise ValueError("No GPUs found")
-
+    
     options = xla_client.generate_pjrt_gpu_plugin_options()
     options["platform_name"] = "ROCM"
     c_api = xb.register_plugin(
         "rocm", priority=500, library_path=str(path), options=options
     )
+    
+    device_count = rocm_plugin_extension.get_device_count()
+    if device_count <= 0:
+        raise ValueError("No GPUs found")
+    
     if rocm_plugin_extension:
         xla_client.register_custom_call_handler(
             "ROCM",
