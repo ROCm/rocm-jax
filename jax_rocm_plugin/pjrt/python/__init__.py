27eb0e27bd38b6c7491569236cc0d9c233effe41
Aleksei <208770786+Arech8@users.noreply.github.com>
2025-12-23 12:22:17 +0200

    Improve check for AMD GPU availability; drop the patchfile (#226)
    Previous check invoked `hipInit()` which led to rocprofiler-sdk misconfiguration (lost GPU events)
    
    This check tests presence of certain KFD files describing properties of compute units and determines if GPUs are there.
    
    Manual testing on both ROCm and CUDA platforms

--- jax_rocm_plugin/pjrt/python/__init__.py
+++ jax_rocm_plugin/pjrt/python/__init__.py
@@ -18,7 +18,9 @@ import functools
 import importlib
 import logging
 import os
+import os.path
 import pathlib
+import re
 
 from jax._src.lib import xla_client  # pylint: disable=import-error
 import jax._src.xla_bridge as xb  # pylint: disable=import-error
@@ -31,7 +33,7 @@ for pkg_name in ["jax_rocm7_plugin", "jax_rocm60_plugin", "jaxlib.rocm"]:
             f"{pkg_name}.rocm_plugin_extension"
         )
     except ImportError:
-        rocm_plugin_extension = None
+        rocm_plugin_extension = None  # pylint: disable=invalid-name
     else:
         break
 
@@ -128,6 +130,58 @@ def set_rocm_paths(path):  # pylint: disable=too-many-branches
     os.environ["JAX_ROCM_PLUGIN_INTERNAL_LLD_PATH"] = lld_path
 
 
+def is_amd_gpu_available() -> bool:
+    """Checks if any AMD GPU is available to safeguard loading of the ROCm pjrt.
+
+    Different approaches to this are possible. We chose testing for existence
+    of KFD kernel driver entities as a proxy for the presence of AMD GPUs as
+    a good compromise between performance, reliability and simplicity.
+    Presence of such entities doesn't guarantee that the GPUs are usable through
+    HIP and PJRT, however, we can't do much much better without spawning an
+    additional process with a potentially complicated setup to run actual HIP
+    code. And we don't want to initialize HIP right now inside the current
+    process, because doing so might spoil a proper initialization of the
+    rocprofiler-sdk later during PJRT startup."""
+
+    try:
+        kfd_nodes_path = "/sys/class/kfd/kfd/topology/nodes/"
+        if not os.path.exists(kfd_nodes_path):
+            return False
+
+        # the RE matches strings like "simd_count ##" and extracts the number ##
+        r_simd_count = re.compile(r"\bsimd_count\s+(\d+)\b", re.MULTILINE)
+        # we're using a non-zero simd_count as a trait of a GPU following the
+        # KFD implementation
+        # https://github.com/torvalds/linux/blob/ea1013c1539270e372fc99854bc6e4d94eaeff66/drivers/gpu/drm/amd/amdkfd/kfd_topology.c#L941
+
+        for node in os.listdir(kfd_nodes_path):
+            node_props_path = os.path.join(kfd_nodes_path, node, "properties")
+            if not os.path.exists(node_props_path):
+                continue
+
+            try:
+                file_size = os.path.getsize(node_props_path)
+                # 16KB is more than a reasonable limit
+                if file_size <= 0 or file_size > 16 * 1024:
+                    continue
+
+                with open(node_props_path, "r", encoding="ascii") as f:
+                    match = r_simd_count.search(f.read())
+                    if match:
+                        simd_count = int(match.group(1))
+                        if simd_count > 0:
+                            return True  # one is enough
+            except Exception as e:  # pylint: disable=broad-exception-caught
+                logger.debug(
+                    "Failed to read KFD node file '%s': %s", node_props_path, e
+                )
+                continue
+
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        logger.warning("Failed to check for AMD KFD presence: %s", e)
+    return False
+
+
 def initialize():
     """Initialize the JAX ROCm plugin."""
     path = _get_library_path()
@@ -139,6 +193,9 @@ def initialize():
     if rocm_plugin_extension is None:
         logger.warning("rocm_plugin_extension not found")
         return
+
+    if not is_amd_gpu_available():
+        raise ValueError("No AMD GPUs were found, skipping ROCm plugin initialization")
 
     options = xla_client.generate_pjrt_gpu_plugin_options()
     options["platform_name"] = "ROCM"
