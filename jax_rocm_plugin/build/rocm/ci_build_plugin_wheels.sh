3eb34eab375569e1d2b9599eb1bd19edce2ee7bd
Alexandros Theodoridis <atheodor@amd.com>
2026-01-19 08:53:04 +0000

    Address review comments

--- jax_rocm_plugin/build/rocm/ci_build_plugin_wheels.sh
+++ jax_rocm_plugin/build/rocm/ci_build_plugin_wheels.sh
@@ -6,6 +6,10 @@ SCRIPT_DIR=$(realpath $(dirname $0))
 declare -A args
 args['--wheelhouse']=""
 args['--rocm_version']=""
+# we use jax_rocm_plugin hash
+# the rest jax, xla are defined as
+# a bazel dependency under third_party dir
+args['--git_hash']=""
 
 BAZEL_ARGS=()
 while [ $# -gt 0 ]; do
@@ -21,13 +25,14 @@ done
 
 WHEELHOUSE=${args['--wheelhouse']}
 ROCM_VERSION=${args['--rocm_version']}
+GIT_HASH=${args['--git_hash']}
 
 bazel --bazelrc=${SCRIPT_DIR}/jax.bazelrc run \
     --config=rocm \
     "${BAZEL_ARGS[@]}" \
     @jax//jaxlib/tools:build_wheel_tool \
     -- \
-    --jaxlib_git_hash=${GIT_HASH} \
+    --jaxlib_git_hash="this lib is built as a part of jax_rocm_plugin build using commit [${GIT_HASH}], please check third_party jax dependency" \
     --output_path="${WHEELHOUSE}" \
     --cpu=x86_64
 
