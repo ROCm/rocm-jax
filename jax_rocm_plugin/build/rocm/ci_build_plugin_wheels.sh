ec4090e60cbcbb80c5db1467986493c4f9dbae6b
Alex <alexandros.theodoridis@amd.com>
2026-01-21 20:40:50 +0100

    Introduce jax nightly tests pipeline with rbe (#247)
    * Introduce nightly rbe run tests pipeline
    
    * Test for different python versions
    
    * Address review comments
    
    * Address review comments
    
    * Clean up patch
    
    * Add support for built jaxlib from @jax instead of upstream whl
    
    * Keep going testing on failure
    
    * Address review comments
    
    * Add options to container in nightly workflow
    
    * Fix the patch
    
    * Drop some targets
    
    * Update nightly-rbe.yml
    
    * Fix tags and legacy create init for python tests
    
    * Assign proper test tags filters
    
    * Unify single_gpu multi_gpu tests
    
    * Add clean up step
    
    * Address review comments
    
    * Remove rbe bazelrc load
    
    * Address review comments
    
    * Remove unused parallel_gpu_execute script
    
    * Address review commetns

---
+++ jax_rocm_plugin/build/rocm/ci_build_plugin_wheels.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+set -ex
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+declare -A args
+args['--wheelhouse']=""
+args['--rocm_version']=""
+# we use jax_rocm_plugin hash
+# the rest jax, xla are defined as
+# a bazel dependency under third_party dir
+args['--git_hash']=""
+
+BAZEL_ARGS=()
+while [ $# -gt 0 ]; do
+    key=$(echo $1 | cut -d "=" -f 1)
+    value=$(echo $1 | cut -d "=" -f 2)
+    if [[ -v "args[$key]" ]]; then
+        args[$key]=$value
+    else
+        BAZEL_ARGS+=($1)
+    fi
+    shift
+done
+
+WHEELHOUSE=${args['--wheelhouse']}
+ROCM_VERSION=${args['--rocm_version']}
+GIT_HASH=${args['--git_hash']}
+
+bazel --bazelrc=${SCRIPT_DIR}/jax.bazelrc run \
+    --config=rocm \
+    "${BAZEL_ARGS[@]}" \
+    @jax//jaxlib/tools:build_wheel_tool \
+    -- \
+    --jaxlib_git_hash="this lib is built as a part of jax_rocm_plugin build using commit [${GIT_HASH}], please check third_party jax dependency" \
+    --output_path="${WHEELHOUSE}" \
+    --cpu=x86_64 \
+    --build_from_external_workspace
+
+bazel --bazelrc=${SCRIPT_DIR}/jax.bazelrc run \
+    --config=rocm \
+    "${BAZEL_ARGS[@]}" \
+    //pjrt/tools:build_gpu_plugin_wheel \
+    -- \
+    --output_path="${WHEELHOUSE}" \
+    --cpu=x86_64 \
+    --enable-rocm=True \
+    --platform_version=${ROCM_VERSION} \
+    --rocm_jax_git_hash=${GIT_HASH}
+
+bazel --bazelrc=${SCRIPT_DIR}/jax.bazelrc run \
+    --config=rocm \
+    "${BAZEL_ARGS[@]}" \
+    //jaxlib_ext/tools:build_gpu_kernels_wheel \
+    -- \
+    --output_path="${WHEELHOUSE}" \
+    --cpu=x86_64 \
+    --enable-rocm=True \
+    --platform_version=${ROCM_VERSION} \
+    --rocm_jax_git_hash=${GIT_HASH}
