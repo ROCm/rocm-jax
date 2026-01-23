409982bd3cd1a660cd2ed9308a69a98aa888c93b
Alexandros Theodoridis <atheodor@amd.com>
2026-01-23 10:20:38 +0000

    Clean up

--- jax_rocm_plugin/build/rocm/ci_run_jax_ut.sh
+++ jax_rocm_plugin/build/rocm/ci_run_jax_ut.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(realpath $(dirname $0))
 
 bazel --bazelrc=${SCRIPT_DIR}/jax.bazelrc test \
     --config=rocm \
-    --@jax//jax:build_jaxlib=false \
+    --@jax//jax:build_jaxlib=plugin_wheels \
     --keep_going \
     --test_verbose_timeout_warnings \
     --local_test_jobs=4 \
