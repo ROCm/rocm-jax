056b3c1d4b699bbe9edc30a12cb650cacefa11cf
Alexandros Theodoridis <atheodor@amd.com>
2026-02-12 14:01:43 +0000

    Add flaky, timeout handling and ignore failing tests

--- jax_rocm_plugin/build/rocm/ci_run_jax_ut.sh
+++ jax_rocm_plugin/build/rocm/ci_run_jax_ut.sh
@@ -10,6 +10,9 @@ bazel --bazelrc="${SCRIPT_DIR}/jax.bazelrc" test \
     --keep_going \
     --test_verbose_timeout_warnings \
     --local_test_jobs=4 \
+    --test_timeout=920,2400,7200,9600 \
+    --test_sharding_strategy=disabled \
+    --flaky_test_attempts=3 \
     --test_output=errors \
     --run_under=@xla//build_tools/rocm:parallel_gpu_execute \
     "$@" \
