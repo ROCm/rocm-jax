0d409700f5460eb1a6aac9791708694409f43678
gulsumgudukbay <gudukbay@gmail.com>
2026-04-10 04:26:48 +0000

    add generic targets
    (cherry picked from commit c56ecbbad6afaffdf3de9a66c3a3781c7b3c4e74)

--- jax_rocm_plugin/build/rocm/setup.rocm.sh
+++ jax_rocm_plugin/build/rocm/setup.rocm.sh
@@ -94,6 +94,6 @@ echo "$ROCM_PATH"
 echo "$GPU_DEVICE_TARGETS"
 
 # Ensure the ROCm target list is set up
-GPU_DEVICE_TARGETS=${GPU_DEVICE_TARGETS:-"gfx908 gfx90a gfx942 gfx950 gfx1030 gfx1100 gfx1101 gfx1200 gfx1201"}
+GPU_DEVICE_TARGETS=${GPU_DEVICE_TARGETS:-"gfx908 gfx90a gfx9-4-generic gfx10-3-generic gfx11-generic gfx12-generic"}
 printf '%s\n' "${GPU_DEVICE_TARGETS}" | tee -a "$ROCM_PATH/bin/target.lst"
 touch "${ROCM_PATH}/.info/version"
